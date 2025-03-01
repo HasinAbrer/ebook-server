c29081effa6c587b813ba7364c69d75fe727a3d7
HasinAbrer <hasinjunior12@gmail.com>
2025-03-02 03:47:39 +0600

    basic typescript setup

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const math_1 = require("./test/math");
+const a = 5;
+const b = 5;
+console.log("This is the final result: ", (0, math_1.add)(a, b));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {add} from '@/test/math'
+
+const a = 5
+const b = 5
+
+console.log ("This is the final result: ", add(a, b))
