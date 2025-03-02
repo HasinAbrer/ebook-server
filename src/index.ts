7cfa14906447c0ca021ec47779a7004994438b03
HasinAbrer <hasinjunior12@gmail.com>
2025-03-02 15:54:06 +0600

    setup .env file in and updated node.js version

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express'
+import express from "express";
 
-const app = express () 
+const app = express();
 
-const port = process.env.PORT || 8989 
+const port = process.env.PORT || 8989;
 
-app.get ('/', (request, response) => {
-    response.send ("<h1>This is our App</h1>")
-})
+app.get("/", (request, response) => {
+  response.send("<h1>This is our App</h1>");
+});
 
-app.get ('/login', (request, response) => {
-    response.send ("<h1>This is our login page</h1>")
-})
+app.get("/login", (request, response) => {
+  response.send("<strong>This is our login page</strong>");
+});
 
-app.listen (port, () => {
-    console.log (`The Application is running on port http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`The Application is running on port http://localhost:${port}`);
+});
