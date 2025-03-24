a0eeaefa5da2f79a2207d800666b27847e243695
HasinAbrer <hasinjunior12@gmail.com>
2025-03-24 17:05:20 +0600

    validate email  with middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,21 @@ import { Router } from "express";
 
 const authRouter = Router();
 
-authRouter.post("/generate-link", generateAuthLink);
+authRouter.post( 
+  "/generate-link",
+  (req, res, next) => {
+    const { email } = req.body;
+    // hasinjunior12@gmail.com (validate the email address)
+    const regex = new RegExp("^[a-z0-9]+[a-z0-9._]*@gmail.com$");
+
+    if (!regex.test(email)) {
+      // sending error response
+      return res.status(422).json({ error: "Invalid Email!" });
+    }
+
+    next();
+  },
+  generateAuthLink
+);
 
 export default authRouter;
