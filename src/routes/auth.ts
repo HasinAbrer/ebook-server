7d755f0f27be3292a670202ceddb56f34e09fb56
HasinAbrer <hasinjunior12@gmail.com>
2025-03-24 17:43:30 +0600

    use zod for validate email address

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,19 +1,37 @@
 import { generateAuthLink } from "src/controllers/auth";
 import { Router } from "express";
+import { z } from "zod";
 
 const authRouter = Router();
 
-authRouter.post( 
+const schema = z.object({
+  email: z
+    .string({
+      required_error: "Email is missing!",
+    })
+    .email("Zod says it is Invalid!"),
+});
+
+authRouter.post(
   "/generate-link",
   (req, res, next) => {
     const { email } = req.body;
-    // hasinjunior12@gmail.com (validate the email address)
-    const regex = new RegExp("^[a-z0-9]+[a-z0-9._]*@gmail.com$");
+    // validate the email address
+    // const regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
 
-    if (!regex.test(email)) {
-      // sending error response
-      return res.status(422).json({ error: "Invalid Email!" });
+    // if (!regex.test(email)) {
+    //   // sending error response
+    //   return res.status(422).json({ error: "Invalid Email!" });
+    // }
+
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      console.log(result);
+      const errors = result.error.flatten().fieldErrors;
+      return res.status(422).json({ errors });
     }
+
+    result.data.email
 
     next();
   },
