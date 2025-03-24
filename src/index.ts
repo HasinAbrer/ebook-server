a3a8dc195aecd863ebffd37c6d49a8794411e4a9
HasinAbrer <hasinjunior12@gmail.com>
2025-03-24 14:59:28 +0600

    add some extensions for formatting and more flexibility

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 8989
+const port = process.env.PORT || 8989;
 
-app.get('/', (request, response) => {
-    response.send ("<h1>This is our App!</h1>")
-})
+app.get("/", (request, response) => {
+  response.send("<h1>This is our App!</h1>");
+});
 
-app.get('/login', (request, response) => {
-    response.send ("<h1>This is login page!</h1>")
-})
+app.get("/login", (request, response) => {
+  response.send("<h1>This is login page!</h1>");
+});
 
 app.listen(port, () => {
-    console.log(`The application is runnning on port http://localhost:${port}`)
-})
+  console.log(`The application is running on port http://localhost:${port}`);
+});
